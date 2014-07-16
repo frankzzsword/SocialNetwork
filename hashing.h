f7a13b078433dc8d05681d77b65e5b596acb0f0c
Boyazhou <zhouboya2012@gmail.com>
2014-07-16 15:27:22 -0700

    first hashing.h

---
+++ hashing.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "person.cpp"
+using namespace std; 
+
+
+#ifndef HASHING_H
+#define HASHING_H
+
+class hashing
+{
+private:
+	static const int tableSize = 100;
+	struct item
+	{
+		person hashedPerson ;
+	    item* next;
+		item() {};
+	};
+
+	item* HashTable[tableSize];
+
+	
+public:
+	hashing(); 
+	int Hash(string key);
+	void addObject(person &personObj);
+	int NumberOfItemsInIndex(int index); //Count the number of people in one bucket
+	void PrintTable();//Print the information in each bucket, and tell us how many people in that bucket
+	void removeObject(person &personObj); // remove the object from hash table
+};
+
+#endif

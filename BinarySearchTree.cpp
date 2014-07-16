9fb2d903d8907b064ec80c183442df7f56331f9d
andrewei88 <andrewei88@gmail.com>
2014-07-16 16:44:15 -0700

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
+#include "BST.h"
 
 using namespace std;
-
-struct BSTNode{
-	int data;
-	BSTNode* left;
-	BSTNode* right;
-};
-
-BSTNode* GetNewNode(int data){
-	BSTNode* newNode = new BSTNode();
-	newNode->data = data;
-	newNode->left = newNode->right = NULL;
-	return newNode;
-}
-
-BSTNode* Insert(BSTNode* root, int data){
-if(root == NULL) {
-	root = GetNewNode(data);
-}
-else if(data <= root->data){
-	root->left = Insert (root->left,data);
-}
-else {
-	root->right = Insert(root->right,data);
-}
-return root;
-}
-
-bool Search(BSTNode* root, int data){
-	if(root == NULL) return false;
-	else if(root->data == data) return true;
-	else if(data <= root->data) return Search(root->left,data);
-	else return Search(root->right, data);
-}
-
 
 int main(){
 BSTNode* root = NULL; // set the tree as empty

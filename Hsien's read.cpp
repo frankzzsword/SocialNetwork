75eaa7a45e374d968a68b8320fcb5ad1be15410f
hsienshien <hsien-hsiacool@hotmail.com>
2014-07-21 18:10:55 -0700

    Update Hsien's read.cpp
    Has change some codes. Thank to Varun's help.
    Now should match to person.h and person.cpp files.

--- Hsien's read.cpp
+++ Hsien's read.cpp
@@ -1,54 +1,53 @@
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <sstream>
+#include "person.h"
+
 using namespace std;
 
-int count();
-void *reRead(int i);
+class READ
+{
+	person personArray[100];
+	int count;
+
+public:
+	void reRead(string textname)  //dynamically allocate memory and storing each product line into the dynamic memory array
+	{
+		count = 0;
+		string  Name;
+		string gender;
+		string age;
+		string address;
+		string school;
+
+		ifstream infile;
+		infile.open(textname);
+		while(!infile.eof())
+		{
+			getline(infile, Name);
+			getline(infile, gender);
+			getline(infile, age);
+			getline(infile, address);
+			getline(infile, school);
+			int Age = stoi(age);
+
+
+			person tempPerson(Name, "none", gender, Age, address, school);
+			personArray[count] = tempPerson;
+	
+			cout<< personArray[count].getName()  << " " <<  personArray[count].getGender() << " " <<  personArray[count].getAge() << " " <<  personArray[count].getCity() << " " <<  personArray[count].getCollege() << endl ;
+
+		}
+		infile.close();
+	}
+};
 
 int main()
 {
-	int i = count();
-	reRead(i);
+	READ r;
+	r.reRead("names.txt");
 	return 0;
-}
-
-int count()  //count the lines and read in each line as a string
-{
-	string temp;
-	ifstream infile;
-	infile.open("names.txt");
-	if (!infile)
-	{
-		cout << "Error opening file\n";
-		return false;
-	}
-	int i = 0;
-	while( !infile.eof() )
-	{
-		getline(infile, temp);
-		i++;
-	}
-	infile.close();
-	cout << "Numbers of line: " << i << endl;
-	return i;
-}
-
-void *reRead(int i)  //dynamically allocate memory and storing each product line into the dynamic memory array
-{
-	string *ptr = new string [i];
-	ifstream infile;
-	infile.open("names.txt");
-	for (int j=0; j < i; j++)
-	{
-		getline(infile, *(ptr+j) );
-		//infile.ignore();
-	}
-	infile.close();
-	for (int z = 0; z < i; z++)
-	{
-		cout << *(ptr+z) << endl;
-	}
-	cout << endl;
-	return ptr;
 }

332775259e0195393708def5c90d223600a5033e
frankzzsword <varunmisra@gmail.com>
2014-07-21 23:43:53 -0700

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -1,4 +1,7 @@
+#ifndef BINARYSEARCHTREE
+#define BINARYSEARCHTREE
 #include <iostream>
+#include "person.h"
 
 using namespace std;
 
@@ -6,20 +9,21 @@ template <class itemType>
 class BinarySearchTree
 {
 private:
-	struct node
-	{
-		itemType data;
-		node* left;
-		node* right;          
-	};
-		node* root;
-
+    struct node
+    {
+        itemType data;
+        node* left;
+        node* right;
+    };
+    node* root;
+    
 public:
-	BinarySearchTree() {root = NULL;}
-	bool isEmpty() const {return root == NULL;} //checks if BST is empty
-	void insert(itemType); //insert an item into the BST
-	void remove(itemType); //removes an item from the BST
-	void inOrder(node*); //calculates the in-order traversal
-	void print_inOrder(); //prints the in-order traversal
-
+    BinarySearchTree() {root = NULL;}
+    bool isEmpty() const {return root == NULL;} //checks if BST is empty
+    void insert(itemType data); //insert an item into the BST
+    void remove(itemType data); //removes an item from the BST
+    void inOrder(node*); //calculates the in-order traversal
+    void print_inOrder(); //prints the in-order traversal
+    
 };
+#endif

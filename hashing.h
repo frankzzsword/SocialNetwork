063e26ba5217bc25416e77d1378e03acd12d2f62
frankzzsword <varunmisra@gmail.com>
2014-07-22 23:08:10 -0700

    Dynamic Array

--- hashing.h
+++ hashing.h
@@ -13,22 +13,25 @@ using namespace std;
 class hashing
 {
 private:
-    static const int tableSize = 10;
+    int tableSize;
     struct item
     {
         person hashedPerson ;
         item* next;
         item() {};
     };
-    item* HashTable[tableSize];
+    item **HashTable;
     
 public:
-    hashing();
+    
+    hashing() { tableSize = 0; };
+    hashing(int size);
     int Hash(string key);
     void addObject(person &personObj);
     int NumberOfItemsInIndex(int index); //Count the number of people in one bucket
     void PrintTable();//Print the information in each bucket, and tell us how many people in that bucket
     void removeObject(person &personObj); // remove the object from hash table
+    void getPrime(int &num);
 };
 
 #endif

19ac5a97d4efabfd0be95a548b8afe6cb0eeaec5
frankzzsword <varunmisra@gmail.com>
2014-07-28 03:48:10 -0700

    Balanced Binary Trees

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -8,32 +8,30 @@ using namespace std;
 template <class itemType>
 class BinarySearchTree
 {
-private:
-    struct node
-    {
-        person *data;
-        node* left;
-        node* right;
-    };
-    node* root;
     
-public:
+    struct node {
+    itemType *data;
+    node* left;
+    node* right;
+    };
+    node *root;
+        
+  public:
     BinarySearchTree() {root = NULL;}
 
     bool isEmpty() const {return root == NULL;} //checks if BST is empty
-    void insert(itemType &data); //insert an item into the BST
-//    typename BinarySearchTree<itemType>::node* insert(node*, itemType &);
-//    void insert(itemType &);
+    typename BinarySearchTree<itemType>::node* _insert(node*&, itemType &);
+    void insert(itemType &);
     void remove(itemType &data); //removes an item from the BST
     void inOrder(node*); //calculates the in-order traversal
     void print_inOrder(); //prints the in-order traversal
-    itemType* _search(node* ,itemType const &d);
+    itemType* _search(node*   ,itemType const &d);
     itemType* search(itemType const &d);
     int Height(node*);
-    void rotateLeftOnce(node*);
-    void rotateLeftTwice(node*);
-    void rotateRightOnce(node*);
-    void rotateRightTwice(node*);
+    void rotateLeftOnce(node*&);
+    void rotateLeftTwice(node*&);
+    void rotateRightOnce(node*&);
+    void rotateRightTwice(node*&);
     
 };
 #endif

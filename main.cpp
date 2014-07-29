dc23edf2f06f939ceef153d4e62c4bf879f543c0
frankzzsword <varunmisra@gmail.com>
2014-07-29 15:28:08 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,30 +11,27 @@
 #include "person.h"
 #include "inputoutput.cpp"
 #include "BinarySearchTree.h"
-
-
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-   
+    
     inputoutput menu;
     menu.readFile("names.txt");
-    
     int input = 0;
     while(input != 2) {
         cout << "\n-------------------Welcome to the Social Network---------------------\n\n\n";
         string name;
         string password;
         cout << "Username: ";
-        cin.ignore();
+        cin.ignore(1,'\n');
         getline(cin, name);
         cout << "Password: ";
         cin>>password;
-        
         if(name != "admin")
-            menu.userMenu(menu.personArray, name, password);
+            menu.userMenu(name, password);
         else
             menu.adminMenu(menu, password);
+    
     }
     
 }

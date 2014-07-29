ade112da751c167f78c8543bc65aaa28a85a3026
frankzzsword <varunmisra@gmail.com>
2014-07-29 16:59:15 -0700

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -23,8 +23,12 @@ class BinarySearchTree
     typename BinarySearchTree<itemType>::node* _insert(node*&, itemType &);
     void insert(itemType &);
     void remove(itemType &data); //removes an item from the BST
+//    typename BinarySearchTree<itemType>::node* _remove(node*,itemType &data); //removes an item from the BST
+//    typename BinarySearchTree<itemType>::node* findPred(node* root);
     void inOrder(node*); //calculates the in-order traversal
     void print_inOrder(); //prints the in-order traversal
+    void printIndent();
+    void indented(node* p, int indent );
     itemType* _search(node*   ,itemType const &d);
     itemType* search(itemType const &d);
     int Height(node*);

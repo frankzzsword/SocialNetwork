fbaf873c2579810e0327b671ba1c4eb3e89c2ad8
Boyazhou <zhouboya2012@gmail.com>
2014-07-28 18:20:00 -0700

    Update hashing.h

--- hashing.h
+++ hashing.h
@@ -1,37 +1,37 @@
-
-#ifndef HASHING_H
-#define HASHING_H
-
-
 #include <iostream>
 #include <cstdlib>
 #include <string>
 #include "person.h"
-using namespace std;
+using namespace std; 
 
+
+#ifndef HASHING_H
+#define HASHING_H
 
 class hashing
 {
 private:
-    int tableSize;
-    struct item
-    {
-        person *hashedPerson ;
-        item* next;
-        item() {};
-    };
-    item **HashTable;
-    
+	int tableSize;
+	struct item
+	{
+		person *hashedPerson ;
+	    item* next;
+		item() { next = NULL; hashedPerson = NULL; };
+	};
+
+	item **HashTable;
+
+	
 public:
-    
-    hashing() { tableSize = 0; };
-    hashing(int size);
-    int Hash(string key);
-    void addObject(person &personObj);
-    int NumberOfItemsInIndex(int index); //Count the number of people in one bucket
-    void PrintTable();//Print the information in each bucket, and tell us how many people in that bucket
-    void removeObject(person &personObj); // remove the object from hash table
-    void getPrime(int &num);
+	hashing() { tableSize = 0; } 
+	hashing(int size);
+	int Hash(string key);
+	void addObject(person &personObj);
+	int NumberOfItemsInIndex(int index); //Count the number of people in one bucket
+	void PrintTable();//Print the information in each bucket, and tell us how many people in that bucket
+	void removeObject(person &personObj); // remove the object from hash table
+	void getPrime(int &num);
+
 };
 
 #endif

c44235769047a6bee929795db836016d3d395a88
frankzzsword <varunmisra@gmail.com>
2014-07-30 16:53:45 -0700

    Update hashing.h

--- hashing.h
+++ hashing.h
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <string>
 #include "person.h"
-using namespace std; 
+using namespace std;
 
 
 #ifndef HASHING_H
@@ -11,35 +11,38 @@ using namespace std;
 class hashing
 {
 private:
-	int tableSize;
-	int numberofentries; //added
-	struct item
-	{
-		person *hashedPerson ;
-	    item* next;
-		item() { next = NULL; hashedPerson = NULL; };
-	};
+    int tableSize;
+    int numberofentries;
+	int largestbucket;
+	int numberofcollisions;
 
-	item **HashTable;
-
-	
+    struct item
+    {
+        person *hashedPerson ;
+        item* next;
+        item() { next = NULL; hashedPerson = NULL; };
+    };
+    
+    item **HashTable;
+    
+    
 public:
-	hashing() { tableSize = 0; } 
-	hashing(int size);
-	int Hash(string key);
-	void addObject(person &personObj);
-	int NumberOfItemsInIndex(int index); //Count the number of people in one bucket
-	void PrintTable();//Print the information in each bucket, and tell us how many people in that bucket
-	void removeObject(person &personObj); // remove the object from hash table
-	void getPrime(int &num);
-
-	void addentry(){numberofentries++;}//added
-	void deleteentry(){numberofentries--;}//added
-	double getLoadingfactor(){return (double)numberofentries/(double)tableSize;}//added
-	int getNumberofentries(){return numberofentries;}
-	int getTablesize(){ return tableSize;}
-	//person getHashtable(int i){return **HashTable;}
-	
+    hashing() { tableSize = 0; }
+    hashing(int size);
+    int Hash(string key);
+    void addObject(person &personObj);
+    int NumberOfItemsInIndex(int index); //Count the number of people in one bucket
+    void PrintTable();//Print the information in each bucket, and tell us how many people in that bucket
+    void removeObject(person &personObj); // remove the object from hash table
+    void getPrime(int &num);
+    
+    void deleteentry(){numberofentries--;}//added
+    double getLoadingfactor(){return (double)numberofentries/(double)tableSize;}
+    int getNumberofentries(){return numberofentries;}
+    int getTablesize(){ return tableSize;}
+    void reHash();
+    //person getHashtable(int i){return **HashTable;}
+    
 };
 
 #endif
